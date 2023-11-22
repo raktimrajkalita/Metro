eee3b51917c66e4a7a7f893bc3619176717ed141
raktimrajkalita <raktimraj.rrk2000@gail.com>
2023-11-22 23:56:17 +0530

    Metro a.3: CSS Create page and figma notify

--- code.ts
+++ code.ts
@@ -1,6 +1,27 @@
 figma.showUI(__html__, { width: 350, height: 600 });
 
-// Your plugin logic goes here. This is a basic structure.
+// Function to send the current frame names to the UI
+function updateUI() {
+    const currentSelection = figma.currentPage.selection;
+    const frameNames = currentSelection
+        .filter(node => node.type === 'FRAME')
+        .map(frame => frame.name);
+
+    figma.ui.postMessage({ type: 'update-frames', frameNames });
+}
+
+figma.notify(`Hey there! 👋`, {
+  timeout: 2000,
+  error: false,
+});
+
+// Call updateUI to set the initial state
+updateUI();
+
+// Update the UI on selection change
+figma.on('selectionchange', updateUI);
+
+// Handle messages from the UI
 figma.ui.onmessage = msg => {
     if (msg.type === 'your-message-type') {
         // Handle the message
